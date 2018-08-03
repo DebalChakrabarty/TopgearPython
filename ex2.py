236475397797af46d72e4a5d100ec626904d5241
Debal Chakrabarty <debalchakrabarty@gmail.com>
2018-08-03 10:12:43 +0530

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+''' 
+Implement the function isWhiteLine(), which takes a string and returns TRUE if the
+string contains only white space & tab characters.
+Making use of the above function, write a program, which should read a file given as
+command-line argument, and print only non-blank lines onto the standard output.
+'''
