41da6a2995db7209c4f6b5b5838e383c2b6cde2d
Debal Chakrabarty <debalchakrabarty@gmail.com>
2018-08-03 10:13:20 +0530

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,12 @@
+'''
+Implement the function isListOfInts(), which takes a list, and checks if the list has
+only "int" type values, as per the specification given below:
+• The function should return True, if the list has only "int" type values, otherwise
+should return False.
+• The function should return True, if an empty list is passed as argument.
+• If the argument is not of ‘list’ type, then the function should throw ValueError
+exception, with the error message 'arg not of <list> type'
+• The function should have only one return statement.
+To test correctness of the function isListOfInts(),implement the function testList()
+which is called as shown below and should produce output as indicated.
+'''
